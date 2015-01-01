3682aa4dc0eb23a84dfcab7cd5d86c73f0be9e6e
Upasana <me@upasana.me>
2015-01-02 02:58:20 +0530

    Changed the way of rendering templates events.tt & index.tt
    This is the sequence:
    1) New event description will be taken from templates/new_event.yaml
    2) templates/events.haml will be modified to add the upcoming event
    3) templates/events.haml will get rendered to events.tt
    4) templates/index.tt will get rendered to index.html
    
    Earlier, index.tt was being rendered to index.html in create.sh
    using tpage, but now it is being rendered in this script

--- bin/render_events.pl
+++ bin/render_events.pl
@@ -3,20 +3,97 @@
 use strict;
 use warnings;
 
-use IO::All;
+use feature qw(say);
+
+use Data::Dumper;
 use Text::Haml;
+use IO::All;
 
-my ($filename) = @ARGV
-    or die "$0 <file.html>\n";
+use Template;
 
-my $haml     = Text::Haml->new;
-my $template = io($filename)->slurp;
-my $output   = $haml->render($template);
+use YAML qw(LoadFile);
 
-print << "_END";
+my $event_ref = LoadFile('new_event.yaml');
+
+my %event     = %$event_ref;
+
+my $month     = $event_ref->{ month };
+my $daynum    = $event_ref->{ daynum };
+my $year      = $event_ref->{ year };
+my $space     = "  ";
+
+my $text =<<END;
+%h2 Upcoming Events
+%div
+  %h3 $month $daynum, $year
+  %ol
+END
+
+my $talks_string;
+for my $key ( keys %{$event_ref->{ talks }} ) {
+    my $talk    = $event_ref->{ talks }->{ $key };
+    my $talk_detail;
+    my $speaker;
+    my $title;
+    if( $talk ) {
+        $speaker    = $talk->{ speaker };
+        $title      = $talk->{ title };
+        my $detail  = $talk->{ detail };
+        if( ref $detail ) {
+            my @detail  = @{$talk->{ detail }};
+
+            for( my $j = 0; $j <= $#detail; $j++ ) {
+                my $line = $detail[ $j ];
+                $talk_detail .= "      %p $line";
+                if( $j != $#detail ) {
+                    $talk_detail .= "\n";
+                }
+            }
+        } else {
+                $talk_detail .= "      %p $detail\n";
+        }
+    } else {
+        last;
+    }
+
+    my $this_talk =<<END;
+    %li
+      %h4
+        %span $speaker
+        :: $title
+$talk_detail
+END
+
+    $talks_string .= $this_talk;
+}
+
+$text .= $talks_string;
+
+my $events_haml = io('events.haml')->slurp();
+$events_haml = $text.$events_haml;
+io('events.haml')->write( $events_haml );
+
+my $haml = Text::Haml->new( encoding => '');
+my $events = $haml->render_file('events.haml');
+
+my $text_for_events_tt =<<END;
 [% WRAPPER root.tt %]
     <h1>Events</h1>
-$output
+$events
 [% END %]
-_END
+END
 
+io('events.tt')->write( $text_for_events_tt );
+
+my $template_name;
+my $rendered_text;
+
+## Render index.tt
+
+my $tt = Template->new ();
+
+$template_name = "index.tt";
+
+$tt->process( $template_name,
+              $event_ref,
+              '../generated/index.html' ) || die $tt->error()."\n";
